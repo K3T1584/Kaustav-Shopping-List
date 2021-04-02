6a3143c7783022b33bb125c765bf2cf9c98e1a42
Kaustav Tamuly <kaustavtamuly@.com>
2021-04-02 14:06:33 +0530

    Version 1.0.4

--- client/src/store.js
+++ client/src/store.js
@@ -28,15 +28,22 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+// const store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
+
+// export default store;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, initialState, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 export default store;
